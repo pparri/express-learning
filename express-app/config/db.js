5bf8015fd6f17ab5e02960000a8abe27b570d68d
pparri <stillalive.8811@gmail.com>
2025-10-31 15:44:10 +0100

    v1.1

--- express-app/config/db.js
+++ express-app/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     
-    const MONGODB_URI = 'mongodb+srv://aleixparrilla_db_user:cPQkZG7rZR1NNaHm@cluster0.pffxqxe.mongodb.net/?appName=Cluster0'
+    const MONGODB_URI = 'your mongodb connection string here';
     //Await es para que espere a que se conecte antes de seguir con el código
     await mongoose.connect(MONGODB_URI).then(() => {
         console.log('Connected to MongoDB');
